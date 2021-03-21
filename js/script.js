e71b066d858d6ec74b2dcb87305403118a1af2cb
Bet-Zua Jimenez <betzuaj@gmail.com>
2021-03-21 11:04:06 -0500

    refactoring & cleaning up search functionality

--- js/script.js
+++ js/script.js
@@ -25,8 +25,7 @@ function showPage(list, page){
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i += 1){
       if(i >= startIndex && i < endIndex){
-         const studentItem = document.createElement('li.student-details');
-         studentItem.innerHTML =  
+         studentList.innerHTML += 
          `<li class="student-item cf">
             <div class="student-details">
                <img class = "avatar" src="${list[i]["picture"]["medium"]}" alt="Profile Picture">
@@ -37,7 +36,6 @@ function showPage(list, page){
                <span class="date">Joined ${list[i]["registered"]["date"]}</span>
             </div>
          </li>`;
-         studentList.insertAdjacentElement("beforeend", studentItem);
       }
    }
 }
@@ -49,16 +47,18 @@ The function takes one parameter, an array of student data.
 */
 
 function addPagination(list){
-   const numButtons = Math.floor(list.length/9);
+   const numButtons = Math.ceil(list.length/9);
    const linkList = document.querySelector('ul.link-list');
    linkList.innerHTML = '';
    for (let i = 1; i <= numButtons; i += 1){
-      const buttonElement = document.createElement('li');
-      buttonElement.innerHTML = `<button type="button">${i}</button>`;
-      linkList.insertAdjacentElement("beforeend", buttonElement);
+      linkList.innerHTML += 
+      `<li>
+         <button type="button">${i}</</button>
+       </li>`;
    }
    const firstButton = document.querySelectorAll('button')[0];
    firstButton.className = 'active';
+   
    linkList.addEventListener('click', (e)=>{
       const clickedElement = e.target;
       if(clickedElement.type == 'button'){
@@ -76,11 +76,10 @@ It also adds functionality by making the search button clickable and filters the
 */
 
 function addSearchBar(list){
-   const page = document.querySelector('div.page');
-   const header = document.querySelector('header');
+   const header = document.querySelector('header'); 
    const searchBar = document.createElement('label');
    const studentList = document.querySelector('ul.student-list');
-   const linkList = document.querySelector('ul.link-list');
+
    searchBar.className = 'student-search'
    searchBar.for = 'search';
    searchBar.innerHTML = 
@@ -106,27 +105,22 @@ function addSearchBar(list){
    const searchButton = document.getElementById('searchButton');
    searchButton.addEventListener('click', (e)=>{
       const search = document.querySelector('input').value.toLowerCase();
-      console.log("button works now!");
       const matches = returnMatches(search);
-      if(matches.length > 0){
-         showPage(matches, 1);
-         addPagination(matches);
-      } else{ 
-         studentList.innerHTML = '';
-         linkList.innerHTML = '';
-         const notFound = document.createElement('h2');
-         notFound.innerHTML = 'No results found';
-         page.insertBefore(notFound, studentList);
+      showPage(matches, 1);
+      addPagination(matches);
+      if(matches.length < 1){
+         studentList.innerHTML = 'No results found';
       }
     });
    //handle real time typing 
    searchBar.addEventListener('keyup', (e)=>{
       const search = document.querySelector('input').value.toLowerCase();
       const matches = returnMatches(search);
-      if(matches.length > 0){
-         showPage(matches, 1);
-         addPagination(matches);
-      } 
+      showPage(matches, 1);
+      addPagination(matches);
+      if(matches.length < 1){
+         studentList.innerHTML = 'No results found';
+      }
    });
 
 }
